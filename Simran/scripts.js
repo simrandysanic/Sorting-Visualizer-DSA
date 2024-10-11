da924386a33dbfa7b96fa7c0547690fbe201727d
Simran Prasad <75943180+simrandysanic@users.noreply.github.com>
2024-10-11 15:40:17 +0530

    script file for index.html

---
+++ Simran/scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navbar = document.getElementById("navbar");
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
+}
